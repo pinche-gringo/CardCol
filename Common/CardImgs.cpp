5ab6e4dafab90632a58fc3ef170b25e26d8e7efe
Markus Schwab <g17m0@lycos.com>
2002-04-01 05:54:41 +0000

    Initial revisioN

---
+++ Common/CardImgs.cpp
@@ -0,0 +1,91 @@
+//$Id$
+
+//PROJECT     : Cardgames
+//SUBSYSTEM   : Common
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision$
+//AUTHOR      : Markus Schwab
+//CREATED     : 29.03.2002
+//COPYRIGHT   : Anticopyright (A) 2002
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#define DEBUG 0
+#include <Check.h>
+#include <Trace_.h>
+
+#include <File.h>
+#include <ANumeric.h>
+
+#include "CardSet.h"
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Constructor; adds all controls to the dialog
+//Parameters: set: Specifier for type of cardset
+/*--------------------------------------------------------------------------*/
+CardSet::CardSet (CardSets set) throw (std::string) : cards (CARDS) {
+   TRACE3 ("CardSet::CardSet (CardSets) - " << (int)set);
+
+   load (set);
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Destructor
+/*--------------------------------------------------------------------------*/
+CardSet::~CardSet () {
+   TRACE9 ("CardSet::~CardSet");
+
+   for (int i = CARDS; i > 0;) {
+      delete cards[--i];
+      cards[i] = NULL;
+   }
+}
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Retrieves the specified cardnumber
+//Parameters: nr: Number of card to retrieve
+/*--------------------------------------------------------------------------*/
+CardWidget* CardSet::getCard (unsigned int nr) const {
+   Check3 (nr < CARDS);
+
+   return cards[nr];
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Constructor; adds all controls to the dialog
+//Parameters: set: Specifier for type of cardset
+/*--------------------------------------------------------------------------*/
+void CardSet::load (CardSets set = NORMAL) throw (std::string) {
+   std::string file (PKGDIR);
+   if (file.empty ())
+      file = ".";
+
+   if (file[file.size () - 1] != File::DIRSEPERATOR)
+      file += File::DIRSEPERATOR;
+
+   std::string temp;
+   ANumeric nr;
+   for (int i = 0; i < CARDS; ++i) {
+      nr = i + 1;
+      temp = file + nr.toUnformatedString () + ".xpm";
+
+      cards[i] = new CardWidget (temp);
+   }
+}
---
+++ Common/CardImgs.h
@@ -0,0 +1,45 @@
+#ifndef CARDSET_H
+#define CARDSET_H
+
+//$Id$
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include <string>
+#include <vector.h>
+
+#include <CardWidget.h>
+
+// Class to display a card on the screen
+class CardSet {
+ public:
+   typedef enum { NORMAL } CardSets;
+
+   CardSet (CardSets set = NORMAL) throw (std::string);
+   ~CardSet ();
+
+   CardWidget* getCard (unsigned int nr) const;
+
+   void load (CardSets set = NORMAL) throw (std::string);
+
+   unsigned int getCardNumber () const { return CARDS; }
+
+ private:
+   vector<CardWidget*> cards;
+
+   static const unsigned int CARDS = 52;  // Number of cards in traditional set
+};
+
+#endif
