2763b8deab31b62c9f8b06151611c885359907d4
Markus Schwab <g17m0@lycos.com>
2002-05-09 02:39:57 +0000

    Initial revision

---
+++ Common/CardSet.cpp
@@ -0,0 +1,87 @@
+//$Id$
+
+//PROJECT     : Cardgames
+//SUBSYSTEM   : Common
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision$
+//AUTHOR      : Markus Schwab
+//CREATED     : 8.5.2002
+//COPYRIGHT   : Anticopyright (A) 2002
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <stdlib.h>
+
+
+#define DEBUG 0
+#include <Check.h>
+#include <Trace_.h>
+
+#include "CardImgs.h"
+#include "CardWidget.h"
+
+#include "CardSet.h"
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Destructor
+/*--------------------------------------------------------------------------*/
+CardSet::~CardSet () {
+   TRACE9 ("CardSet::~CardSet ()");
+}
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds a set of cards (with images specified by decks) to set
+//Parameters: decks: Class holding the images to add
+/*--------------------------------------------------------------------------*/
+void CardSet::addPacket (const CardImages& decks) {
+   TRACE9 ("CardSet::addPacket (const CardImages&)");
+
+   for (int i (0); i < decks.cards (); ++i) {
+      CardWidget* card (new CardWidget (decks, i, true)); Check3 (card);
+
+      cards_.push_back (card);
+   } // endfor
+
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Shuffles the cards in the deck
+/*--------------------------------------------------------------------------*/
+void CardSet::shuffle () {
+   TRACE1 ("CardSet::shuffle ()");
+
+   unsigned int nr;
+   for (int i (cards ()); i > 0;) {
+      nr = rand () & --i;
+      TRACE2 ("CardSet::shuffle () - " << i << " = " << nr);
+      swap (cards_[i], cards_[nr]);
+   }
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Retrieves the specified card
+//Retruns   : CardWidget&: Reference to CardWidget
+/*--------------------------------------------------------------------------*/
+CardWidget& CardSet::getCard (unsigned int nr) const {
+   TRACE3 ("CardSet::getCard (unsigned int) - " << nr);
+   Check3 (nr < cards ()); Check3 (cards_[nr]);
+
+   return *cards_[nr];
+}
---
+++ Common/CardSet.h
@@ -0,0 +1,47 @@
+#ifndef CARDSET_H
+#define CARDSET_H
+
+//$Id$
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include <string>
+#include <vector.h>
+
+
+class CardWidget;
+class CardImages;
+
+
+// Class to hold the cards used in a game
+class CardSet {
+ public:
+   CardSet () { }
+   CardSet (const CardImages& decks) { addPacket (decks); }
+   ~CardSet ();
+
+   CardWidget& getCard (unsigned int nrCard) const;
+   const vector<CardWidget*>& getCards () const { return cards_; }
+   unsigned int cards () const { return cards_.size (); }
+
+   void shuffle ();
+
+   void addPacket (const CardImages& decks);
+
+ private:
+   vector<CardWidget*> cards_;
+};
+
+#endif
