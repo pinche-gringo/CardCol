876c53161050e886e4211708fba23e1b594612ba
Markus Schwab <g17m0@lycos.com>
2002-05-17 04:14:02 +0000

    CardPile&-Collection merged -> Baseclass and template with 2 specializations

--- Common/CardPile.cpp
+++ Common/CardPile.cpp
@@ -27,14 +27,13 @@
 
 #define DEBUG 0
 #include <Check.h>
-#include <Trace_.h>
 
 #include "CardWidget.h"
 
 #include "CardPile.h"
 
 
-static const int SIZE_COMPRESSED = 15;
+static const int SIZE_COMPRESSED = 17;
 
 
 /*--------------------------------------------------------------------------*/
@@ -42,30 +41,28 @@ static const int SIZE_COMPRESSED = 15;
 //Parameters: set: Specifier for type of cardset
 //            access: Pile (topmost card) can be accessed
 /*--------------------------------------------------------------------------*/
-CardPile::CardPile (Style s, bool access) : style (s), accessable (access) {
-   TRACE3 ("CardPile::CardPile (Style) - " << (int)style);
+ICardPile::ICardPile (Style s, bool access) : style (s), accessable (access) {
+   TRACE3 ("ICardPile::ICardPile (Style) - " << (int)style);
    Check3 (s < LAST);
 }
 
 /*--------------------------------------------------------------------------*/
 //Purpose   : Destructor
 /*--------------------------------------------------------------------------*/
-CardPile::~CardPile () {
-   TRACE9 ("CardPile::~CardPile ()");
+ICardPile::~ICardPile () {
+   TRACE9 ("ICardPile::~ICardPile ()");
 }
 
 /*--------------------------------------------------------------------------*/
 //Purpose   : Sets the top card of the pile
 //Parameters: newCard: New top-card
 /*--------------------------------------------------------------------------*/
-void CardPile::setTopCard (CardWidget& card) {
-   TRACE5 ("CardPile::setTopCard (CardWidget&) - New size: " << cards.size () + 1);
-
-   pack_start (card, false);
+void ICardPile::setTopCard (CardWidget& card) {
+   TRACE5 ("ICardPile::setTopCard (CardWidget&) - New size: " << cards.size () + 1);
 
    if ((style > NORMAL) && cards.size ()) {
       Check3 (cards[cards.size () - 1]);
-      getTopCard ().set_usize (-1, style == COMPRESSED ? SIZE_COMPRESSED : 1);
+      resize (getTopCard (), style);
    }
 
    card.set_sensitive (accessable);
@@ -77,17 +74,18 @@ void CardPile::setTopCard (CardWidget& card) {
 //Purpose   : Returns and removes the top card of the pile
 //Returns   : CardWidget&: Reference to (removed) card
 /*--------------------------------------------------------------------------*/
-CardWidget& CardPile::removeTopCard () {
-   TRACE5 ("CardPile::removeTopCard () - New size: " << cards.size () - 1);
+CardWidget& ICardPile::removeTopCard () {
+   TRACE5 ("ICardPile::removeTopCard () - New size: " << cards.size () - 1);
 
    Check3 (cards.size () > 0); Check3 (cards[cards.size () - 1]);
    CardWidget& card (getTopCard ());
-   remove (card);
    cards.pop_back ();
 
    if (cards.size () && (style > NORMAL)) {
+      TRACE9 ("ICardPile::removeTopCard () - Resizing");
+
       CardWidget& card (getTopCard ());
-      card.set_usize (-1, 96);
+      resize (card, NORMAL);
       card.set_sensitive (false);
    }
 
@@ -99,7 +97,7 @@ CardWidget& CardPile::removeTopCard () {
 /*--------------------------------------------------------------------------*/
 //Purpose   : Flips the topmost card of the pile
 /*--------------------------------------------------------------------------*/
-void CardPile::flipTopCard () {
+void ICardPile::flipTopCard () {
    Check3 (cards.size () > 0);
 
    getTopCard ().flip ();
@@ -109,7 +107,7 @@ void CardPile::flipTopCard () {
 //Purpose   : Sets the top card of the pile visible as indicated
 //Parameters: visible: Flag if cardface should be shown or back
 /*--------------------------------------------------------------------------*/
-void CardPile::setTopCardVisible (bool visible) {
+void ICardPile::setTopCardVisible (bool visible) {
    Check3 (cards.size () > 0);
 
    getTopCard ().setVisible (visible);
@@ -119,7 +117,7 @@ void CardPile::setTopCardVisible (bool visible) {
 //Purpose   : Adds various cards to the pile
 //Parameters: visible: Flag if cardface should be shown or back
 /*--------------------------------------------------------------------------*/
-void CardPile::setTopCards (const vector<CardWidget*>& staple) {
+void ICardPile::setTopCards (const vector<CardWidget*>& staple) {
    vector<CardWidget*>::const_iterator i;
 
    for (i = staple.begin (); i != staple.end (); ++i) {
@@ -132,7 +130,7 @@ void CardPile::setTopCards (const vector<CardWidget*>& staple) {
 //Purpose   : Adds various cards to the pile
 //Parameters: visible: Flag if cardface should be shown or back
 /*--------------------------------------------------------------------------*/
-void CardPile::setTopCards (const vector<CardWidget*>& staple, bool visible) {
+void ICardPile::setTopCards (const vector<CardWidget*>& staple, bool visible) {
    vector<CardWidget*>::const_iterator i;
 
    for (i = staple.begin (); i != staple.end (); ++i) {
@@ -144,7 +142,7 @@ void CardPile::setTopCards (const vector<CardWidget*>& staple, bool visible) {
 /*--------------------------------------------------------------------------*/
 //Purpose   : Removes all cards from pile
 /*--------------------------------------------------------------------------*/
-void CardPile::clear () {
+void ICardPile::clear () {
    while (cards.size ()) {
       remove (getTopCard ());
       cards.pop_back ();
@@ -156,7 +154,7 @@ void CardPile::clear () {
 //Parameters: id: ID of card to return
 //Returns   : CardWidget*: Pointer to card with passed ID (or NULL)
 /*--------------------------------------------------------------------------*/
-CardWidget* CardPile::getCard (unsigned int id) const {
+CardWidget* ICardPile::get (unsigned int id) const {
    vector<CardWidget*>::const_iterator i;
 
    for (i = cards.begin (); i != cards.end (); ++i) {
@@ -174,8 +172,93 @@ CardWidget* CardPile::getCard (unsigned int id) const {
 //Parameters: id: ID of card to return
 //Returns   : CardWidget*: Pointer to card with passed ID (or NULL)
 /*--------------------------------------------------------------------------*/
-void CardPile::setAccessable (bool access) {
+void ICardPile::setAccessable (bool access) {
    accessable = access;
    if (cards.size ())
       getTopCard ().set_sensitive (access);
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Inserts a card into the pile
+//Parameters: card: Card to insert
+/*--------------------------------------------------------------------------*/
+void ICardPile::insert (CardWidget& card, unsigned int pos) {
+   TRACE5 ("ICardPile::insertCard (CardWidget&) - " << pos);
+   Check3 (pos <= cards.size ());
+
+   card.show ();
+
+   if ((style > NORMAL) && cards.size ()) {     // Cards to display compressed?
+      CardWidget* compressCard (pos == cards.size ()
+                                ? cards[cards.size () - 1] : &card);
+      Check3 (compressCard);
+      resize (*compressCard, COMPRESSED);
+   }
+   cards.insert (cards.begin () + pos, &card);
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Removes the passed card from the collection
+//Parameters: card: Card to remove
+/*--------------------------------------------------------------------------*/
+CardWidget& ICardPile::remove (CardWidget& card) {
+   Check3 (cards.size () > 0);
+
+   // Search for card and remove it
+   vector<CardWidget*>::iterator i (find (cards.begin (), cards.end (), &card));
+   Check3 (i != cards.end ());
+
+   // Check if we have to resize a card
+   if (style > NORMAL) {
+      // If last card was removed: Resize new last card (if any)
+      if (((i + 1) == cards.end ()) && i != cards.begin ())
+         resize (**(i - 1), NORMAL);
+      else
+         resize (card, NORMAL);                    // Else enlarge removed card
+   }
+
+   cards.erase (i--);
+
+   return card;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Removes the card at the passed position from the collection
+//Parameters: card: Card(position) to remove
+/*--------------------------------------------------------------------------*/
+CardWidget& ICardPile::remove (unsigned int pos) {
+   Check3 (cards.size () > pos);
+
+   // Remove card on passed position it
+   vector<CardWidget*>::iterator i (cards.begin () + pos);
+   CardWidget* pTemp (*i); Check3 (pTemp);
+
+   // Check if we have to resize a card
+   if (style > NORMAL) {
+      // If last card was removed: Resize new last card (if any)
+      if (((i + 1) == cards.end ()) && i != cards.begin ())
+         resize (**(i - 1), NORMAL);
+      else
+         resize (*pTemp, NORMAL);                  // Else enlarge removed card
+   }
+
+   cards.erase (i--);
+   return *pTemp;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Changes the drawing-style of the collection
+//Parameters: s: New style
+/*--------------------------------------------------------------------------*/
+void ICardPile::setStyle (Style s) {
+   TRACE5 ("ICardPile::setStyle (Style) - Size = " << cards.size ());
+
+   if (s == style)
+      return;
+
+   style = s;
+   for (int i (0); i < static_cast <int> (cards.size () - 1); ++i) {
+      Check3 (cards[i]);
+      resize (*cards[i], style);
+   }
 }
--- Common/CardPile.h
+++ Common/CardPile.h
@@ -22,22 +22,23 @@
 
 #include <gtk--/box.h>
 
+#include <Trace_.h>
+
+
 class CardWidget;
 
 // Class to display a pile of cards on the screen
-class CardPile : public Gtk::VBox {
+class ICardPile {
  public:
-   typedef enum { NORMAL, COMPRESSED, VERY_COMPRESSED, LAST } Style;
+   typedef enum { NORMAL = 0, COMPRESSED, VERY_COMPRESSED, LAST } Style;
 
-   CardPile (Style style = NORMAL, bool access = true);
-   virtual ~CardPile ();
+   ICardPile (Style style = NORMAL, bool access = true);
+   virtual ~ICardPile ();
 
-   void setStyle (Style s) { style = s; }
-   void setAccessable (bool access);
-
+   // Methods to access pile first-in-last-out
    void flipTopCard ();
    void setTopCardVisible (bool visible = true);
-   void setTopCardInVisible () { setTopCardVisible (false); }
+   void setTopCardInvisible () { setTopCardVisible (false); }
 
    void setTopCards (const vector<CardWidget*>& staple);
    void setTopCards (const vector<CardWidget*>& staple, bool visible);
@@ -45,28 +46,93 @@ class CardPile : public Gtk::VBox {
       setTopCards (staple, visible);
       setTopCardVisible (lastVisible); }
 
-   void setTopCard (CardWidget& newCard);
+   virtual void setTopCard (CardWidget& newCard);
    void setTopCard (CardWidget& newCard, bool visible) {
       newCard.setVisible (visible);
       setTopCard (newCard); }
 
    CardWidget& getTopCard () { return *cards[cards.size () - 1]; }
-   CardWidget& removeTopCard ();
-   void clear ();
+   virtual CardWidget& removeTopCard ();
 
-   CardWidget* getCard (unsigned int id) const;
+   // Methods to random access pile
+   virtual void insert (CardWidget& card, unsigned int pos);
+   void append (CardWidget& card) { setTopCard (card); }
+   virtual CardWidget& remove (CardWidget& card);
+   virtual CardWidget& remove (unsigned int pos);
 
-   bool existCard (CardWidget& card) const { existCard (&card); }
-   bool existCard (CardWidget* card) const {
+   CardWidget* get (unsigned int id) const;
+   CardWidget& at (unsigned int pos) const { return *cards[pos]; }
+
+   bool exist (CardWidget& card) const { exist (&card); }
+   bool exist (CardWidget* card) const {
       return find (cards.begin (), cards.end (), card) != cards.end (); }
 
+   // General management-functions
+   virtual void resize (CardWidget& card, Style s) const = 0;
    unsigned int numberOfCards () const { return cards.size (); }
+   void clear ();
+   void setStyle (Style s);
+   void setAccessable (bool access);
 
  private:
    vector<CardWidget*> cards;
-
    Style style;
    bool accessable;
 };
+
+
+// Specializations of ICardPile to display. The template must support:
+//
+//   - pack_start, remove, reorder_child - Methods
+//
+// Designed to be used with the Gtk::?Box-classes
+template <class T> class CardPile : public T, public ICardPile {
+ public:
+   CardPile (Style style = NORMAL, bool access = true) : ICardPile (style, access) { }
+   ~CardPile () { }
+
+   virtual void setTopCard (CardWidget& newCard) {
+      ICardPile::setTopCard (newCard);
+      pack_start (newCard, false); }
+   void setTopCard (CardWidget& newCard, bool visible) {
+      ICardPile::setTopCard (newCard, visible); }
+
+   virtual CardWidget& removeTopCard () {
+      CardWidget& card (ICardPile::removeTopCard ());
+      T::remove (card);
+      return card; }
+
+   virtual void insert (CardWidget& card, unsigned int pos) {
+      ICardPile::insert (card, pos);
+      pack_start (card, false);
+      reorder_child (card, pos); }
+
+   virtual CardWidget& remove (CardWidget& card) {
+      T::remove (ICardPile::remove (card));
+      return card; }
+   virtual CardWidget& remove (unsigned int pos) {
+      CardWidget& card (ICardPile::remove (pos));
+      T::remove (card);
+      return card; }
+
+   virtual void resize (CardWidget& card, Style s) const { }
+};
+
+
+typedef CardPile<Gtk::VBox>  CardVPile;
+typedef CardPile<Gtk::HBox>  CardHPile;
+
+
+void CardVPile::resize (CardWidget& card, Style s) const {
+   static unsigned int sizes[(int)LAST] = { 96, 15, 1 };
+   TRACE5 ("CardVPile::resize (CardWidget&, Style) - " << (int)s);
+   card.set_usize (-1, sizes[(int)s]);
+}
+
+void CardHPile::resize (CardWidget& card, Style s) const {
+   static unsigned int sizes[(int)LAST] = { 72, 18, 1 };
+   TRACE5 ("CardHPile::resize (CardWidget&, Style) - " << (int)s);
+   card.set_usize (sizes[(int)s], -1);
+}
 
 #endif
