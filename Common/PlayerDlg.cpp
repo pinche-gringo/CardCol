4ba1bebbadb4521e3a33400d09f1d3607640d81e
Markus Schwab <g17m0@lycos.com>
2003-12-17 05:02:19 +0000

    Including cardgames-cfg.h and not YGP/Internal.h

--- Common/PlayerDlg.cpp
+++ Common/PlayerDlg.cpp
@@ -25,7 +25,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
-#include <YGP/Internal.h>
+#include <cardgames-cfg.h>
 
 #include <gtkmm/box.h>
 #include <gtkmm/label.h>
