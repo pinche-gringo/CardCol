717720ae4a4011b43a1a4f465908803b8123d0e7
Markus Schwab <g17m0@lycos.com>
2005-04-28 23:37:07 +0000

    Initial revision

---
+++ src/GameTypes.h
@@ -0,0 +1,41 @@
+#ifndef GAMETYPES_H
+#define GAMETYPES_H
+
+//$Id$
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <YGP/MetaEnum.h>
+
+
+/**Class to store the different game-types; both as string and as (unique)
+   numeric representation
+*/
+class GameTypes : public YGP::MetaEnum {
+ public:
+   enum { NONE = -1, ROVHULT = 0, TWOPART, HEARTS, BURACO, MACHIAVELLI,
+	  SGTMAYOR, LAST };
+
+   GameTypes ();
+   ~GameTypes ();
+
+ private:
+   GameTypes (const GameTypes& other);
+
+   const GameTypes& operator= (const GameTypes& other);
+};
+
+#endif
