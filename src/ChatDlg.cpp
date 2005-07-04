c33353c5cfcf6d754bc6df7b920d93f8f3ae0684
Markus Schwab <g17m0@lycos.com>
2005-07-04 14:38:52 +0000

    Initial revision

---
+++ src/ChatDlg.cpp
@@ -0,0 +1,110 @@
+//$Id$
+
+//PROJECT     : Cardgames
+//SUBSYSTEM   : ChatDlg
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision$
+//AUTHOR      : Markus Schwab
+//CREATED     : 2005-06-29
+//COPYRIGHT   : Copyright (C) 2005
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <cardgames-cfg.h>
+
+#include <gtkmm/box.h>
+#include <gtkmm/entry.h>
+#include <gtkmm/button.h>
+#include <gtkmm/textview.h>
+
+#define CHECK 9
+#define TRACELEVEL 9
+#include <YGP/Check.h>
+#include <YGP/Trace.h>
+
+#include "ChatDlg.h"
+
+
+//-----------------------------------------------------------------------------
+/// Constructor
+//-----------------------------------------------------------------------------
+ChatDlg::ChatDlg ()
+   : XGP::XDialog (CANCEL), txtMsg (new Gtk::Entry), tvMsgs (new Gtk::TextView),
+     msgs (Gtk::TextBuffer::create ()) {
+   TRACE9 ("ChatDlg::ChatDlg ()");
+   set_title (_("Chat"));
+
+   tvMsgs->set_editable (false);
+   tvMsgs->set_buffer (msgs);
+
+   Check2 (get_vbox ());
+   get_vbox ()->pack_start (*manage (tvMsgs), Gtk::PACK_EXPAND_WIDGET, 5);
+   get_vbox ()->pack_start (*manage (txtMsg), Gtk::PACK_SHRINK, 5);
+
+   Check2 (get_action_area ());
+   Gtk::Button& send (*manage (new Gtk::Button (_("_Send"), true)));
+   get_action_area ()->pack_start (send, Gtk::PACK_SHRINK, 5);
+   send.can_default ();
+   send.grab_default ();
+   send.signal_clicked ().connect (mem_fun (*this, &ChatDlg::sendMessage));
+
+   txtMsg->grab_focus ();
+   send.grab_default ();
+
+   tagSender = msgs->create_tag ("Sender");
+   tagSender->property_foreground () = "blue";
+
+   set_default_size (300, 150);
+   show_all_children ();
+   show ();
+}
+
+//-----------------------------------------------------------------------------
+/// Destructor
+//-----------------------------------------------------------------------------
+ChatDlg::~ChatDlg () {
+   TRACE9 ("ChatDlg::~ChatDlg ()");
+}
+
+//-----------------------------------------------------------------------------
+/// Callback after clicking the send-button
+//-----------------------------------------------------------------------------
+void ChatDlg::sendMessage () {
+   Check1 (txtMsg);
+   Check2 (txtMsg->get_text_length ());
+   TRACE1 ("ChatDlg::sendMessage () - " << txtMsg->get_text ());
+
+   signalSend.emit (txtMsg->get_text ());
+   txtMsg->set_text ("");
+}
+
+//-----------------------------------------------------------------------------
+/// Adds a message to the message-list
+/// \param sender: Sender of message
+/// \param msg: Sent message
+//-----------------------------------------------------------------------------
+void ChatDlg::addMessage (const Glib::ustring& sender, const Glib::ustring& msg) {
+   TRACE3 ("ChatDlg::addMessage (2x const Glib::ustring&) - " << sender << ": " << msg);
+   Glib::RefPtr<Gtk::TextMark> pos (msgs->create_mark (msgs->end ()));
+   msgs->insert (msgs->end (), sender);
+   msgs->apply_tag (tagSender, pos->get_iter (), msgs->end ());
+
+   msgs->insert (msgs->end (), _(": "));
+   msgs->insert (msgs->end (), msg);
+   msgs->insert (msgs->end (), "\n");
+}
---
+++ src/ChatDlg.h
@@ -0,0 +1,73 @@
+ #ifndef CHATDLG_H
+#define CHATDLG_H
+
+//$Id$
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <glibmm/refptr.h>
+
+#include <XGP/XDialog.h>
+
+
+namespace Gtk {
+   class Entry;
+   class TextTag;
+   class TextView;
+   class TextBuffer;
+}
+
+
+/**Dialog to enter a message to send to others and to display messages received
+ * from others.
+ *
+ * The dialog emits a signal when a message is to send.
+ */
+class ChatDlg : public XGP::XDialog {
+ public:
+   virtual ~ChatDlg ();
+
+   /// Creates the dialog
+   /// \param parent: Parent window
+   static ChatDlg* create (const Glib::RefPtr<Gdk::Window>& parent) {
+      ChatDlg* dlg (new ChatDlg);
+      dlg->get_window ()->set_transient_for (parent);
+      dlg->signal_response ().connect (mem_fun (*dlg, &ChatDlg::free));
+      return dlg;
+   }
+
+   /// Signal emitted, when OK is selected
+   sigc::signal<void, const Glib::ustring&> signalSend;
+
+   void addMessage (const Glib::ustring& sender, const Glib::ustring& msg);
+
+ protected:
+   ChatDlg ();
+   void sendMessage ();
+
+ private:
+   //Prohibited manager functions
+   ChatDlg (const ChatDlg& other);
+   const ChatDlg& operator= (const ChatDlg& other);
+
+   Gtk::Entry*    txtMsg;
+   Gtk::TextView* tvMsgs;
+
+   Glib::RefPtr<Gtk::TextTag>    tagSender;
+   Glib::RefPtr<Gtk::TextBuffer> msgs;
+};
+
+#endif
