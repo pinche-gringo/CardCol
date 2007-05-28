54ef9ac85ff7ddb94ecc023af2d8ea515c49a938
Markus Schwab <g17m0@lycos.com>
2007-05-28 09:32:05 +0000

    Added getCompressedSize() to CardPile-template

--- Common/CardPile.h
+++ Common/CardPile.h
@@ -239,6 +239,8 @@ template <class T> class CardPile : public T, public ICardPile {
          resize (size () - 1, style);
          ICardPile::sort (fnSort);
          resortGUI (); } }
+   static unsigned int getCompressedSize (PileStyle s) { return 0; }
+   unsigned int getCompressedSize () { return getCompressedSize (style); }
 
  protected:
    virtual void resortGUI () {
@@ -254,29 +256,37 @@ typedef CardPile<Gtk::VBox>  CardVPile;
 typedef CardPile<Gtk::HBox>  CardHPile;
 
 
+/// Implementation of the getCompressionRate() method for Gtk::VBox
+template <> inline unsigned int CardVPile::getCompressedSize (PileStyle s) {
+   int height[(int)LAST] = { CardImages::HEIGHT, 15, 7, 1 };
+   return height[s];
+}
 /// Implementation of the resize() methods for Gtk::VBox
 template <> inline void CardVPile::resize (unsigned int pos, PileStyle s) {
    ICardPile::resize (pos, s); }
 template <> inline void CardVPile::resize (CardWidget& card, PileStyle s) {
-   if (s == TOTALLY_COMPRESSED)
+   if (s >= TOTALLY_COMPRESSED)
       card.hide ();
    else {
-      int height[(int)LAST] = { card.getImageHeight (), 15, 7, 1 };
-      card.set_size_request (-1, height[s]);
+      card.set_size_request (-1, getCompressedSize (s));
       if (style == TOTALLY_COMPRESSED)
          card.show ();
    }
 }
 
+/// Implementation of the getCompressionRate() method for Gtk::VBox
+template <> inline unsigned int CardHPile::getCompressedSize (PileStyle s) {
+   int width[(int)LAST] = { CardImages::WIDTH, 18, 7, 1 };
+   return width[s];
+}
 ///Implementation of the resize() methods for Gtk::HBox
 template <> inline void CardHPile::resize (unsigned int pos, PileStyle s) {
    ICardPile::resize (pos, s); }
 template <> inline void CardHPile::resize (CardWidget& card, PileStyle s) {
-   if (s == TOTALLY_COMPRESSED)
+   if (s >= TOTALLY_COMPRESSED)
       card.hide ();
    else {
-      int width[(int)LAST] = { card.getImageWidth (), 18, 7, 1 };
-      card.set_size_request (width[s], -1);
+      card.set_size_request (getCompressedSize (s), -1);
       if (style == TOTALLY_COMPRESSED)
          card.show ();
    }
